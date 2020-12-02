af13b1d9a4827da42f1be772f4530e4427a2ea47
Sasha <aleksikunya@mail.ru>
2020-12-02 16:23:13 +0300

    4,5,6,7 tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let name = prompt('Ваше имя:'),
+/*let name = prompt('Ваше имя:'),
     age = prompt('Ваш возраст:'),
     city = prompt('Ваш город проживания:'),
     phone = prompt('Ваш телефон:'),
@@ -11,16 +11,79 @@ let year = 2020,
     birthyear = year - age;
 
 document.write('\n \n' + name + ' родился в ' + birthyear + ' году.');
+*/
+let arr = [3, 5, 1, 6, 0, 4],
+    summ1 = arr[0] + arr[1] + arr[2],
+    summ2 = arr[3] + arr[4] + arr[5];
 
-let a = [3, 5, 1, 8, 5, 3],
-    summ1 = a[0] + a[1] + a[2],
-    summ2 = a[3] + a[4] + a[5];
-
-console.log(a);
+console.log(arr);
 console.log(summ1);
 console.log(summ2);
+
 if (summ1 == summ2) {
     console.log('Да');
 } else {
     console.log('Нет');
 }
+
+let n = 1;
+
+if (n > 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let a = 10,
+    b = 2,
+    summ = a + b;
+
+console.log(summ);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+if (summ > 1) console.log(Math.pow(summ, 2));
+
+if ( (a > 2 && a < 11) || (b >= 6 && b < 14) ) { 
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+let minute = prompt('Введите число от 0 до 59:');
+
+if (minute < 0 || minute > 59) {
+    alert('Неверное число');
+} else if (minute < 16) {
+    alert('Первая четверть часа');
+}
+else if (minute < 31 && minute > 15) {
+    alert('Вторая четверть часа');
+}
+else if (minute < 46 && minute > 30) {
+    alert('Третья четверть часа');
+} else {
+    alert('Четвертая четверть часа');
+}
+
+
+/*let minute = prompt('Введите число от 0 до 59:');
+
+switch (minute) {
+    case minute < 0:
+    case minute > 59:
+        alert('Неверное число');
+    break;
+    case minute < 16:
+        alert('Первая четверть часа');
+    break;
+    case 15 < minute < 31:
+        alert('Вторая четверть часа');
+    break;
+    case 30 < minute < 46:
+        alert('Первая четверть часа');
+    break;
+    case 45 < minute < 60:
+        alert('Первая четверть часа');
+    break;
+}*/
